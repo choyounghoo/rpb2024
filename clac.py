6cc497f714f08c93ea4dcffaf2d3052e1d57c019
choyounghoo <165999372+choyounghoo@users.noreply.github.com>
2024-04-04 23:56:29 +0900

    Update clac.py

--- clac.py
+++ clac.py
@@ -1,5 +1,46 @@
+print("Hello World")
+
+def hello():
+    print("Hello World")
+hello()
+
+import myPythonFile
+
+myPythonFile.func()
+
 def main():
+    print("Let's implement addition. Type two numbers for x and y.")
 
-def add():
+    x = int(input("x > "))
+    y = int(input("y > "))
+    
+    print("%d + %d = %d" % (x, y, add(x, y)))    
+    
+def add(x, y):
+    return x+y
 
-def devide():
+if __name__ == "__main__":
+    main()
+
+def main():
+    print("Let's implement division. Type two numbers for x and y")
+    
+    x = int(input("x > "))
+    y = int(input("y > "))
+    
+    print("%d / %d = %0.3f" % (x, y, divide(x, y)))
+    
+    
+def add(a, b):
+    return a + b
+    
+def divide(x, y):
+    if y==0:
+       print("Error: cannot divide by zero.")
+    
+    else:
+       return x/y        
+
+if __name__ == "__main__":
+    main()
+
